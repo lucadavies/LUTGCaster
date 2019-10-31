b5f1742c353ab8e7da33bf7425e459d0f2253130
Luca Davies <luca-davies@outlook.com>
2019-10-31 17:57:55 +0000

    Started generating UI dynamically

--- LUTGCaster/SetupCasting.cs
+++ LUTGCaster/SetupCasting.cs
@@ -12,6 +12,10 @@ namespace LUTGCaster
 {
     public partial class SetupCasting : Form
     {
+        int setupShows = 0;
+        List<Show> shows = new List<Show>();
+        List<GroupBox> showBoxes = new List<GroupBox>();
+
         public SetupCasting()
         {
             InitializeComponent();
@@ -27,98 +31,80 @@ namespace LUTGCaster
         /// </summary>
         private void GenerateCastingSheet()
         {
-            List<Show> shows = new List<Show>();
-            if (!txtS1Name.Text.Equals(""))
+            shows = new List<Show>();
+
+            foreach (GroupBox g in showBoxes)
             {
-                Show s = new Show(txtS1Name.Text);
-                foreach (Control el in panS1.Controls)
+                Panel p = new Panel();
+                TextBox t = new TextBox();
+                foreach (Control c in g.Controls)
                 {
-                    if (!((TextBox)el).Text.Equals(""))
+                    if (c is Panel)
                     {
-                        s.addRole(((TextBox)el).Text);
+                        p = (Panel)c;
+                    }
+                    else if (c is TextBox)
+                    {
+                        t = (TextBox)c;
                     }
                 }
-                shows.Add(s);
+
+                Show s = new Show(t.Text);
+                s.addRole("Dave");
             }
-            if (!txtS2Name.Text.Equals(""))
-            {
-                Show s = new Show(txtS2Name.Text);
-                foreach (Control el in panS2.Controls)
-                {
-                    if (!((TextBox)el).Text.Equals(""))
-                    {
-                        s.addRole(((TextBox)el).Text);
-                    }
-                }
-                shows.Add(s);
-            }
-            if (!txtS3Name.Text.Equals(""))
-            {
-                Show s = new Show(txtS3Name.Text);
-                foreach (Control el in panS3.Controls)
-                {
-                    if (!((TextBox)el).Text.Equals(""))
-                    {
-                        s.addRole(((TextBox)el).Text);
-                    }
-                }
-                shows.Add(s);
-            }
-            if (!txtS4Name.Text.Equals(""))
-            {
-                Show s = new Show(txtS4Name.Text);
-                foreach (Control el in panS4.Controls)
-                {
-                    if (!((TextBox)el).Text.Equals(""))
-                    {
-                        s.addRole(((TextBox)el).Text);
-                    }
-                }
-                shows.Add(s);
-            }
+
+            //if (!txtS1Name.Text.Equals(""))
+            //{
+            //    Show s = new Show(txtS1Name.Text);
+            //    foreach (Control el in panS1.Controls)
+            //    {
+            //        if (!((TextBox)el).Text.Equals(""))
+            //        {
+            //            s.addRole(((TextBox)el).Text);
+            //        }
+            //    }
+            //    shows.Add(s);
+            //}
+            //if (!txtS2Name.Text.Equals(""))
+            //{
+            //    Show s = new Show(txtS2Name.Text);
+            //    foreach (Control el in panS2.Controls)
+            //    {
+            //        if (!((TextBox)el).Text.Equals(""))
+            //        {
+            //            s.addRole(((TextBox)el).Text);
+            //        }
+            //    }
+            //    shows.Add(s);
+            //}
+            //if (!txtS3Name.Text.Equals(""))
+            //{
+            //    Show s = new Show(txtS3Name.Text);
+            //    foreach (Control el in panS3.Controls)
+            //    {
+            //        if (!((TextBox)el).Text.Equals(""))
+            //        {
+            //            s.addRole(((TextBox)el).Text);
+            //        }
+            //    }
+            //    shows.Add(s);
+            //}
+            //if (!txtS4Name.Text.Equals(""))
+            //{
+            //    Show s = new Show(txtS4Name.Text);
+            //    foreach (Control el in panS4.Controls)
+            //    {
+            //        if (!((TextBox)el).Text.Equals(""))
+            //        {
+            //            s.addRole(((TextBox)el).Text);
+            //        }
+            //    }
+            //    shows.Add(s);
+            //}
 
 
             CastingSheet cs = new CastingSheet(shows);
             cs.Show();
-        }
-
-        private void NumUDShows_ValueChanged(object sender, EventArgs e)
-        {
-            EnableDisableShowBoxes();
-        }
-
-        /// <summary>
-        /// Enables / disables show box based on value of selector for numebr of shows
-        /// </summary>
-        private void EnableDisableShowBoxes()
-        {
-            switch (numUDShows.Value)
-            {
-                case 1:
-                    EnableShowBox(gBoxS1, true);
-                    EnableShowBox(gBoxS2, false);
-                    EnableShowBox(gBoxS3, false);
-                    EnableShowBox(gBoxS4, false);
-                    break;
-                case 2:
-                    EnableShowBox(gBoxS1, true);
-                    EnableShowBox(gBoxS2, true);
-                    EnableShowBox(gBoxS3, false);
-                    EnableShowBox(gBoxS4, false);
-                    break;
-                case 3:
-                    EnableShowBox(gBoxS1, true);
-                    EnableShowBox(gBoxS2, true);
-                    EnableShowBox(gBoxS3, true);
-                    EnableShowBox(gBoxS4, false);
-                    break;
-                case 4:
-                    EnableShowBox(gBoxS1, true);
-                    EnableShowBox(gBoxS2, true);
-                    EnableShowBox(gBoxS3, true);
-                    EnableShowBox(gBoxS4, true);
-                    break;
-            }
         }
 
         /// <summary>
@@ -150,6 +136,93 @@ namespace LUTGCaster
                 
             }
             gb.Enabled = enabled;
-        }        
+        }
+
+        private void AddShow()
+        {
+            GroupBox gBox = new GroupBox
+            {
+                Location = new Point(12, 12 + (76 * (setupShows))),
+                Name = "gBoxS" + (setupShows + 1),
+                Size = new Size(320, 70),
+                TabIndex = 7,
+                TabStop = false,
+                Text = "Show " + (setupShows + 1)
+            };
+
+            Label lblName = new Label
+            {
+                AutoSize = true,
+                Location = new Point(6, 16),
+                Name = "lblName" + (setupShows + 1),
+                Size = new Size(35, 13),
+                TabIndex = 1,
+                Text = "Name"
+            };
+
+            Label lblChar = new Label
+            {
+                AutoSize = true,
+                Location = new Point(6, 42),
+                Name = "lblChar" + (setupShows + 1),
+                Size = new Size(58, 13),
+                TabIndex = 2,
+                Text = "Characters"
+            };
+
+            TextBox txtSName = new TextBox
+            {
+                Location = new Point(70, 13),
+                Name = "txtS1Name",
+                Size = new Size(244, 20),
+                TabIndex = 0
+            };
+
+            Panel panChars = new Panel
+            {
+                Location = new Point(70, 39),
+                Name = "panS1",
+                Size = new Size(300, 21),
+                TabIndex = 28
+            };
+
+            TextBox txtChar1 = new TextBox
+            {
+                Location = new Point(0, 0),
+                Margin = new Padding(0, 3, 0, 3),
+                Name = "txtS" + (setupShows + 1) + "C1",
+                Size = new Size(122, 20),
+                TabIndex = 3
+            };
+
+            TextBox txtChar2 = new TextBox
+            {
+                Location = new Point(122, 0),
+                Margin = new Padding(0, 3, 0, 3),
+                Name = "txtS" + (setupShows + 1) + "C1",
+                Size = new Size(122, 20)
+            };
+
+            gBox.Controls.Add(panChars);
+            gBox.Controls.Add(lblName);
+            gBox.Controls.Add(lblChar);
+            gBox.Controls.Add(txtSName);
+            panChars.Controls.Add(txtChar1);
+            panChars.Controls.Add(txtChar2);
+            Controls.Add(gBox);
+
+            showBoxes.Add(gBox);
+            setupShows += 1;
+        }
+
+        private void AddCharacter(int show)
+        {
+             
+        }
+
+        private void btnAddShow_Click(object sender, EventArgs e)
+        {
+            AddShow();
+        }
     }
 }
